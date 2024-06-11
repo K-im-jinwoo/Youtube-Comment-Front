470b40181036eda339d04630ef629ad19d326258
김진우 <zaqaz113@naver.com>
2024-06-11 17:20:56 +0900

    Feat: common color 추가

---
+++ src/styles/common.ts
@@ -0,0 +1,42 @@
+export const COLOR = {
+  main: "#3E5EE0",
+
+  white: "#fff",
+
+  gray: {
+    textPrimary: "#D9D9D9",
+    textSecondary: "#9D9D9D",
+    textTeritiary: "#5C5C5C",
+    textDisabled: "#8B8B8B",
+    bgDisabled: "4A4A4A",
+    icons: "#6C6C6C",
+    stroke: "#3A3A3A",
+    divider: "#323232",
+  },
+  violet: {
+    accentPrimary: "#4264DF",
+    accentSecondary: "#6782E7",
+    accentTertiary: "#95AAF5",
+    accentQuarternary: "#C0CAF1",
+    btnHover: "#244AD0",
+    btnClick: "#244DDF",
+  },
+  orange: {
+    accentPrimary: "F37B38",
+    accentSecondary: "EA996B",
+    accentTertiary: "FFAB7C",
+    accentQuarternary: "FFC2A0",
+  },
+  system: {
+    successHover: "369926",
+    success: "4DA63E",
+    successSecondary: "B0F9A5",
+    successTertiary: "C2F5B9",
+    attentionHover: "EFD02C",
+    attentionTertiary: "F9F1C7",
+    errorHover: "ED2F2F",
+    error: "FF4545",
+    errorSecondary: "F7B7B7",
+    errorTertiary: "FBDFDF",
+  },
+};
